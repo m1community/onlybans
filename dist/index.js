6f1a5cccdf254acf3fbeffdc3bdc063c02a096af
stemount <stemount@gmail.com>
2021-08-23 02:21:05 +0100

    FUCK OFF

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-app.get("/banned/:person", (req, res) => {
+app.get("/bans/:person", (req, res) => {
     res.status(403).send({
         username: `${req.params.person}`,
         banned: true,
@@ -11,11 +11,11 @@ app.get("/banned/:person", (req, res) => {
 app.get("/bans", (_req, res) => {
     res.status(406).send(`<h1>406 Unaccepted</h1><p>Use /banned/USERNAME silly!</p>`);
 });
-app.get("/bans/:person", (_req, res) => {
+app.get("/ban/:person", (_req, res) => {
     res.status(403).send(`<h1>403 Forbidden</h1><p>Only God can use this function.</p>`);
 });
 app.get("/", (_req, res) => {
-    res.redirect("/banned");
+    res.redirect("/bans");
 });
 app.listen(3000);
 //# sourceMappingURL=index.js.map
