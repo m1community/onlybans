c4ffbd786ee7e52bfa21960292df1a7b48d9aa4e
stemount <stemount@gmail.com>
2021-08-23 02:34:45 +0100

    fuck it FUCK FUCK IT ITITTITIT

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ app.get("/bans/:person", (req, res) => {
     });
 });
 app.get("/bans", (_req, res) => {
-    res.status(406).send(`<h1>406 Unaccepted</h1><p>Use /banned/USERNAME silly!</p>`);
+    res.status(406).send(`<h1>406 UNACCEPTABLE HAX</h1><p>Use /bans/USERNAME, silly!</p>`);
 });
 app.get("/ban/:person", (_req, res) => {
     res.status(403).send(`<h1>403 Forbidden</h1><p>Only God can use this function.</p>`);
@@ -17,5 +17,5 @@ app.get("/ban/:person", (_req, res) => {
 app.get("/", (_req, res) => {
     res.redirect("/bans");
 });
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.get("/bans/:person", (req: Request, res: Response) => {
 });
 
 app.get("/bans", (_req: Request, res: Response) => {
-  res.status(406).send(`<h1>406 Unaccepted</h1><p>Use /banned/USERNAME silly!</p>`);
+  res.status(406).send(`<h1>406 UNACCEPTABLE HAX</h1><p>Use /bans/USERNAME, silly!</p>`);
 });
 
 app.get("/ban/:person", (_req: Request, res: Response) => {
